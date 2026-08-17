1dd3d208374c82fc0943b5f2d24d64e0bf5c028b
danielherakovic-spec <Danielherakovic@gmail.com>
2026-08-17 22:24:36 +0200

    Update config.js
    Verbinden

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Diese Werte sind für Browser bestimmt. Hier niemals einen Secret/Service-Role-Key eintragen.
 window.AUTOVALUE_CONFIG = Object.freeze({
   supabaseUrl: 'https://tyfscjaormfzerbjxzal.supabase.co', 'https://YOUR_PROJECT_REF.supabase.co',
-  supabasePublishableKey:'sb_publishable_BDpiMZRdZL28QJZlPpRgsg_82CJ2HvI', 
+  supabasePublishableKey: 'sb_publishable_BDpiMZRdZL28QJZlPpRgsg_82CJ2HvI', 
   'YOUR_PUBLISHABLE_OR_ANON_KEY',
   photoBucket: 'vehicle-photos',
 
